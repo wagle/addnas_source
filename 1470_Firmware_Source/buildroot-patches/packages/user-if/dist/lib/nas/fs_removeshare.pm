fe3bf7139fbea6fc1412e520e37f80b724a8db2e
Perry Wagle <wagle@mac.com>
2013-05-19 17:50:30 -0700

    reindent lib/nas/fs_removeshare.pm

--- 1470_Firmware_Source/buildroot-patches/packages/user-if/dist/lib/nas/fs_removeshare.pm
+++ 1470_Firmware_Source/buildroot-patches/packages/user-if/dist/lib/nas/fs_removeshare.pm
@@ -15,27 +15,23 @@ use nasCommon;
 
 use Data::Dumper;
 
-
 sub main($$$) {
-
-	my ($self, $cgi, $config) = @_;
-
-	{
-		if ($cgi->param('nextstage') == 1) {
-			$self->stage1($cgi, $config);
-			last;
-		}
-
+  my ($self, $cgi, $config) = @_;
+  {
+    if ($cgi->param('nextstage') == 1) {
+      $self->stage1($cgi, $config);
+      last;
+    }
+    #
     # List the shares 
     #
-		$self->outputTemplate(
-            'fs_removeshare.tpl', 
-            {
-                tabon  => 'fileshare',
-                shares => [ grep( $_->{name} !~ /^public$/i,  @{ $self->getShares($config) } )  ]
-            } );
+    $self->outputTemplate(
+			  'fs_removeshare.tpl', 
+			  {
+			   tabon  => 'fileshare',
+			   shares => [ grep( $_->{name} !~ /^public$/i,  @{ $self->getShares($config) } )  ]
+			  } );
   }
-
 }
 
 #
@@ -43,67 +39,65 @@ sub main($$$) {
 #
 sub stage1($$$) {
 
-	my ($self, $cgi, $config) = @_;
+  my ($self, $cgi, $config) = @_;
 
-	my $vars = { tabon => 'fileshare' };
-	my $error = 0;
-    my $path;
+  my $vars = { tabon => 'fileshare' };
+  my $error = 0;
+  my $path;
 
-	my $sharename = $cgi->param('sharename');
-	my $confirm = $cgi->param('confirm');
+  my $sharename = $cgi->param('sharename');
+  my $confirm = $cgi->param('confirm');
 
-	if ($sharename eq '') {
-		nasCommon::setErrorMessage( $vars, $config, 'sharename', 'e13002' );
-		$error = 1;
-	}
+  if ($sharename eq '') {
+    nasCommon::setErrorMessage( $vars, $config, 'sharename', 'e13002' );
+    $error = 1;
+  }
 
-	if ($confirm ne getMessage($config, 'm13005')) {
-		nasCommon::setErrorMessage( $vars, $config, 'confirm', 'e13001' );
-		$error = 1;
-	}
+  if ($confirm ne getMessage($config, 'm13005')) {
+    nasCommon::setErrorMessage( $vars, $config, 'confirm', 'e13001' );
+    $error = 1;
+  }
 
-	if ($error) {
-        copyFormVars($cgi, $vars);
-        $vars->{shares} = $self->getShares($config);
-		$self->outputTemplate('fs_removeshare.tpl', $vars);
-		return;
-	}
+  if ($error) {
+    copyFormVars($cgi, $vars);
+    $vars->{shares} = $self->getShares($config);
+    $self->outputTemplate('fs_removeshare.tpl', $vars);
+    return;
+  }
 
-    # Set the CIFS permissions on the shares
-    #
-    my $sharesInc = new Config::IniFiles( -file => nasCommon->shares_inc );
+  # Set the CIFS permissions on the shares
+  #
+  my $sharesInc = new Config::IniFiles( -file => nasCommon->shares_inc );
     
-    if ($sharesInc->SectionExists($sharename)) {
+  if ($sharesInc->SectionExists($sharename)) {
 
-        $path = $sharesInc->val($sharename, 'path');
+    $path = $sharesInc->val($sharename, 'path');
         
-        $sharesInc->DeleteSection($sharename);
+    $sharesInc->DeleteSection($sharename);
         
-        unless ($sharesInc->RewriteConfig) {
-            $self->fatalError($config, 'f00013');
-            return;
-        }
-        unless (sudo("$nbin/reconfigSamba.sh")) {
-          $self->fatalError($config, 'f00034');
-          return;
-        }
-
-
+    unless ($sharesInc->RewriteConfig) {
+      $self->fatalError($config, 'f00013');
+      return;
     }
+    unless (sudo("$nbin/reconfigSamba.sh")) {
+      $self->fatalError($config, 'f00034');
+      return;
+    }
+  }
 
-    # Remove the unix share directory - but only for internal shares, not external!
-#    if ($path =~ /^$sharesHome\/internal\/.+/) {
-        # Take away the top dir prefix. Let the rmShareDir script sort out the full path
-        #
-        my $dirName = $path;
-        $dirName =~ s/^$sharesHome\///;
-        unless (sudo("$nbin/rmShareDir.sh $dirName")) {
-            $self->fatalError($config, 'f00021');
-            return;
-        }
-#    }
+  # Remove the unix share directory - but only for internal shares, not external!
+  #    if ($path =~ /^$sharesHome\/internal\/.+/) {
+  # Take away the top dir prefix. Let the rmShareDir script sort out the full path
+  #
+  my $dirName = $path;
+  $dirName =~ s/^$sharesHome\///;
+  unless (sudo("$nbin/rmShareDir.sh $dirName")) {
+    $self->fatalError($config, 'f00021');
+    return;
+  }
+  #    }
 
-    print $cgi->redirect('/auth/fileshare.pl');
+  print $cgi->redirect('/auth/fileshare.pl');
 
 }
 
